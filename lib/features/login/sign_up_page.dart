25712cc9dd46a5f5e10abde6e2a95f963b71513c
BrooksRen8198 <brooksren@cycmotor.com>
2025-11-05 15:54:10 +0800

    feat(create account): 完成创建账号页面UI

--- lib/features/login/sign_up_page.dart
+++ lib/features/login/sign_up_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tra_x/common/global.dart';
 import 'package:tra_x/common/widget/button.dart';
+import 'package:tra_x/common/widget/textField.dart';
 import 'package:video_player/video_player.dart';
 
 /// 创建账号
@@ -36,6 +37,7 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         fit: StackFit.expand,
         children: [
@@ -57,22 +59,37 @@ class _SignUpPageState extends State<SignUpPage> {
 
   Widget _signUpWidget() {
     return SafeArea(
-      child: Padding(
+      child: SingleChildScrollView(
         padding: const EdgeInsets.all(24),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             // 返回按钮
-            GestureDetector(
-              onTap: () => Navigator.of(context).pop(),
-              child: SvgPicture.asset('assets/images/back_sign_up.svg', width: 35, height: 35),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () => Navigator.of(context).pop(),
+                  child: SvgPicture.asset('assets/images/back_sign_up.svg', width: 35, height: 35),
+                ),
+              ],
             ),
             const SizedBox(height: 40),
+            // title
             Text('Create an Account', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
             const SizedBox(height: 20),
+            // google login
             _logoButton('assets/images/logo googleg 48dp.png', 'Continue with Google'),
             SizedBox(height: 10),
+            // facebook login
             _logoButton('assets/images/Facebook Logo.png', 'Continue with Facebook'),
+            Padding(padding: const EdgeInsets.symmetric(vertical: 40), child: _orWidget()),
+            // input
+            TraXTextField(labelText: 'Email', hintText: '', onPressed: () {}),
+            const SizedBox(height: 40),
+            _signupButton(),
+            // By continuing, you are agreeing to our Terms of Services and Privacy Policy.
+            const SizedBox(height: 24),
+            _termsOfService(),
           ],
         ),
       ),
@@ -95,6 +112,54 @@ class _SignUpPageState extends State<SignUpPage> {
           Text(
             text,
             style: TextStyle(color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 暂时复制login_page的代码,等以后复用组件
+  Widget _orWidget() {
+    return Row(
+      children: [
+        Expanded(child: Divider(color: Color(0xFF5E5E5E))),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text('OR', style: TextStyle(color: Color(0xFF949494), fontSize: 14)),
+        ),
+        Expanded(child: Divider(color: Color(0xFF5E5E5E))),
+      ],
+    );
+  }
+
+  Widget _signupButton() {
+    return MyElevatedButton(
+      borderRadius: Global.traXborderRadius,
+      minimumSize: const Size(170, 51),
+      backgroundColor: WidgetStateProperty.all<Color?>(Color(0xFFC0C0C0)),
+      onPressed: () {},
+      child: Text(
+        'Sign up',
+        style: TextStyle(color: Colors.black, fontSize: 24.0, fontWeight: FontWeight.w700),
+      ),
+    );
+  }
+
+  Widget _termsOfService() {
+    return Text.rich(
+      TextSpan(
+        style: TextStyle(fontSize: 14, color: Color(0xFFDADADA)),
+        children: [
+          TextSpan(text: 'By continuing, you are agreeing to our '),
+          // 下划线
+          TextSpan(
+            text: 'Terms of Services',
+            style: TextStyle(decoration: TextDecoration.underline, decorationColor: Color(0xFFDADADA)),
+          ),
+          TextSpan(text: ' and '),
+          TextSpan(
+            text: 'Privacy Policy.',
+            style: TextStyle(decoration: TextDecoration.underline, decorationColor: Color(0xFFDADADA)),
           ),
         ],
       ),
